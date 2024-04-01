645458771a45ef29751f95e7dcb02eba2c44806b
unknown <ossipeerivermediagroup@outlook.com>
2024-04-01 11:16:22 -0400

    improvements on day and night themes

--- app/src/main/java/com/ossipeeriver/ravenandroidawarenesskit/ui/slideshow/SlideshowViewModel.kt
+++ app/src/main/java/com/ossipeeriver/ravenandroidawarenesskit/ui/slideshow/SlideshowViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class SlideshowViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "Forecast Data"
     }
     val text: LiveData<String> = _text
 }
